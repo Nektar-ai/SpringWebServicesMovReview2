db8bbd06b3bd66ab27fd4b3b90dde045d161c924
FonctionZ3R0 <donovan.damhec@gmail.com>
2021-03-30 22:43:32 +0200

    ta moum tu vas passer

--- SpringMovReview/src/main/java/fr/epsi/SpringMovReview/service/MovieService.java
+++ SpringMovReview/src/main/java/fr/epsi/SpringMovReview/service/MovieService.java
@@ -1,14 +1,10 @@
 package fr.epsi.SpringMovReview.service;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 //import fr.epsi.moviereview2.repository.MovieRepository;
@@ -25,13 +21,14 @@ public class MovieService {
 		return data;
 	}
 	
-	public String loadJson () throws IOException, URISyntaxException {
+	public String loadJson () throws IOException
+	{
 		/* TMDB API Key : 719886a0b8020a1ae30c9c5d174c01d3
 		 * Exemple API request : https://api.themoviedb.org/3/movie/550?api_key=719886a0b8020a1ae30c9c5d174c01d3
 		 * API Read Access Token (v4 auth) : eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTk4ODZhMGI4MDIwYTFhZTMwYzljNWQxNzRjMDFkMyIsInN1YiI6IjYwMzhjMTlhMzIyYjJiMDA1NTI1NzkzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jYvJTMpwaUNXjTBCXcBEsze0sLtJjUMaKlAyYgpM2fQ
 		 */
-		Resource resource = new ClassPathResource("static/moviejson.txt");
-        String stringSon = Files.readString(Path.of(resource.getURI()));
+        Path fileName = Path.of("static/moviejson.txt");     
+        String stringSon = Files.readString(fileName);
         return stringSon;
 	}
 }
