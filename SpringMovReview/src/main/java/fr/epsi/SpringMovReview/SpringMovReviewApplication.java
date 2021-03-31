5c2003a36ac22fb7347cd9331a116ebd0bf39730
Cassius P <287ilias@gmail.com>
2021-03-31 02:12:42 +0200

    MongoTemplate in app

--- SpringMovReview/src/main/java/fr/epsi/SpringMovReview/SpringMovReviewApplication.java
+++ SpringMovReview/src/main/java/fr/epsi/SpringMovReview/SpringMovReviewApplication.java
@@ -1,5 +1,6 @@
 package fr.epsi.SpringMovReview;
 
+import fr.epsi.SpringMovReview.repository.FilmRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,6 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.web.client.RestTemplate;
 
 import fr.epsi.SpringMovReview.apifetch.MovieRestTemplate;
@@ -15,6 +22,7 @@ import fr.epsi.SpringMovReview.apifetch.MovieRestTemplate;
 public class SpringMovReviewApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringMovReviewApplication.class);
+	private FilmRepository repository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMovReviewApplication.class, args);
@@ -31,5 +39,17 @@ public class SpringMovReviewApplication {
 			MovieRestTemplate mvrt = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?api_key=719886a0b8020a1ae30c9c5d174c01d3&language=fr-FR", MovieRestTemplate.class);
 			log.info(mvrt.toString());
 		};
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoMappingContext context) {
+
+		MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+
+		return mongoTemplate;
+
 	}
 }
