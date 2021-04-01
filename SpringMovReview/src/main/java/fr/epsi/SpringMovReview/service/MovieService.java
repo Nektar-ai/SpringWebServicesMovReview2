7dac449ef978a4c41c8096c7418b2763650bce45
FonctionZ3R0 <donovan.damhec@gmail.com>
2021-04-01 20:32:20 +0200

    Fix first like

--- SpringMovReview/src/main/java/fr/epsi/SpringMovReview/service/MovieService.java
+++ SpringMovReview/src/main/java/fr/epsi/SpringMovReview/service/MovieService.java
@@ -44,6 +44,7 @@ public class MovieService {
 		Film f2 = movieRepository.findByIdFilm(f.idFilm);
 		if (f2 == null)
 		{
+			f.setLikes(1);
 			movieRepository.save(f);
 		} else {
 			f2.setLikes(f2.getLikes()+1);
@@ -56,6 +57,7 @@ public class MovieService {
 		Film f2 = movieRepository.findByIdFilm(f.idFilm);
 		if (f2 == null)
 		{
+			f.setLikes(-1);
 			movieRepository.save(f);
 		} else {
 			f2.setLikes(f2.getLikes()-1);
